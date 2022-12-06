54ae856fb0820f4c8bea4ec51d447e643c62af90
Sandrine14000 <108656015+Sandrine14000@users.noreply.github.com>
2022-12-06 09:25:07 +0100

    Formulaire Connexion HTML

---
+++ templating/formConnexion.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+    <form action="" method="post">
+    <label for="pseudo">Pseudo</label>
+    <input type="text" name="pseudo" id=""><br>
+    <label for="mdp">Mot de Passe</label>
+    <input type="text" name="mdp" id="mdp">
+    <input type="button" value="OK">
+    </form>
+</body>
+</html>
