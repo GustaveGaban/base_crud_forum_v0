2039c93fe77a316f691c222eac34a3a1d9d02957
Sandrine14000 <108656015+Sandrine14000@users.noreply.github.com>
2022-12-07 09:00:10 +0100

    header

---
+++ templating/footer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+<body>
+    <div class="footer">
+        <div>
+            Site créé par Florent - Victor - Sandrine DWWM 2022
+        </div>
+    </div>
+</body>
+</html>
---
+++ templating/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <style>
+@import url('https://fonts.googleapis.com/css2?family=Kenia&family=Zen+Dots&display=swap');
+</style>
+    
+</head>
+<body>
+    <div class="conteneurAccueil">
+
+        <div class="logo">
+
+            <img id="logo"src="../images/Logo_forum.jpg" alt="Logo du forum">
+
+        </div>
+
+        <div class="titre">
+            
+            <h1>BLABLA_POST</h1><br>
+
+        </div>
+
+        <div class="nav">
+            <a href="formInscription.php" type="button">Inscription</a>
+            <a href="formConnexion.php" type="button">Connexion</a><br>
+
+        </div>
+            
+        <div class="rechercher"> 
+                <input type="text" name="search" id="rechercher" placeholder="🔍">
+                <input type="submit" value="Ok">
+        </div>
+
+    </div>
+</body>
+</html>
